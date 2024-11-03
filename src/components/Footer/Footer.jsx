18c2939d1e67fcad08dd30317946b0aa641c17f4
Han Hsun Shih (Amy) <a23514788@gmail.com>
2024-11-03 15:04:46 +0000

    Add footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import "./Footer.scss";
+
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <p className="footer__title">Copyright Amy © 2024 All Rights Reserved</p>
+    </footer>
+  );
+};
